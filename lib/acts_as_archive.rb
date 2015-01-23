b8ab47eeea19b4ef35bd41f432d6c9edfdbabab3
Ryan Winograd <ryan@thewinograds.com>
2015-01-22 18:27:53 -0600

    Update for Rails 4

--- lib/acts_as_archive.rb
+++ lib/acts_as_archive.rb
@@ -10,9 +10,9 @@ $:.unshift File.dirname(__FILE__)
 
 class ActsAsArchive
   class <<self
-    
+
     attr_accessor :configuration, :disabled
-    
+
     def deprecate(msg)
       if defined?(::ActiveSupport::Deprecation)
         ::ActiveSupport::Deprecation.warn msg
@@ -20,7 +20,7 @@ class ActsAsArchive
         $stdout.puts msg
       end
     end
-    
+
     def disable(&block)
       @mutex ||= Mutex.new
       @mutex.synchronize do
@@ -30,7 +30,7 @@ class ActsAsArchive
     ensure
       self.disabled = false
     end
-    
+
     def find(from)
       from = [ from ] unless from.is_a?(::Array)
       (@configuration || []).select do |hash|
@@ -41,7 +41,7 @@ class ActsAsArchive
         end
       end
     end
-    
+
     def load_from_yaml(root)
       if File.exists?(yaml = "#{root}/config/acts_as_archive.yml")
         YAML.load(File.read(yaml)).each do |klass, config|
@@ -59,7 +59,7 @@ class ActsAsArchive
         end
       end
     end
-    
+
     def move(config, where, merge_options={})
       options = config[:options].dup.merge(merge_options)
       if options[:conditions]
@@ -69,12 +69,12 @@ class ActsAsArchive
       end
       config[:from].move_to(config[:to], options)
     end
-    
+
     def update(*args)
       deprecate "ActsAsArchive.update is deprecated and no longer necessary."
     end
   end
-  
+
   module Base
     def self.included(base)
       unless base.included_modules.include?(InstanceMethods)
@@ -86,12 +86,12 @@ class ActsAsArchive
     module ClassMethods
       def acts_as_archive(options={})
         return unless ActsAsArchive.find(self).empty?
-        
+
         ActsAsArchive.configuration ||= []
         ActsAsArchive.configuration << (config = { :from => self })
-        
+
         options[:copy] = true
-        
+
         if options[:archive]
           options[:magic] = 'restored_at'
           klass = options[:class]
@@ -101,31 +101,31 @@ class ActsAsArchive
           options[:ignore] = options[:magic]
           options[:subtract] = 'restored_at'
           options[:timestamps] = false if options[:timestamps].nil?
-          
+
           unless options[:class]
             options[:class] = "#{self}::Archive"
           end
-          
+
           unless options[:table]
             options[:table] = "archived_#{self.table_name}"
           end
-          
+
           klass = eval(options[:class]) rescue nil
-          
+
           if klass
-            klass.send :set_table_name, options[:table]
+            klass.send :table_name=, options[:table]
           else
             eval <<-EVAL
               class ::#{options[:class]} < ActiveRecord::Base
-                set_table_name "#{options[:table]}"
+                self.table_name = "#{options[:table]}"
               end
             EVAL
             klass = eval("::#{options[:class]}")
           end
-          
+
           klass.record_timestamps = options[:timestamps].inspect
           klass.acts_as_archive(:class => self, :archive => true)
-        
+
           self.reflect_on_all_associations.each do |association|
             if association.options[:dependent] && !association.options[:polymorphic] && !ActsAsArchive.find(association.klass).empty?
               opts = association.options.dup
@@ -134,7 +134,7 @@ class ActsAsArchive
               klass.send association.macro, association.name, opts
             end
           end
-        
+
           unless options[:migrate] == false
             AlsoMigrate.configuration ||= []
             AlsoMigrate.configuration << options.merge(
@@ -143,19 +143,19 @@ class ActsAsArchive
             )
           end
         end
-        
+
         config[:to] = klass
         config[:options] = options
       end
-      
+
       def delete_all!(*args)
         ActsAsArchive.disable { self.delete_all(*args) }
       end
-      
+
       def destroy_all!(*args)
         ActsAsArchive.disable { self.destroy_all(*args) }
       end
-      
+
       def migrate_from_acts_as_paranoid
         time = Benchmark.measure do
           ActsAsArchive.find(self).each do |config|
@@ -171,59 +171,52 @@ class ActsAsArchive
         $stdout.puts "-- #{self}.migrate_from_acts_as_paranoid"
         $stdout.puts "   -> #{"%.4fs" % time.real}"
       end
-      
+
       def restore_all(*args)
         ActsAsArchive.deprecate "#{self}.restore_all is deprecated, please use #{self}.delete_all."
         self.delete_all *args
       end
     end
-    
+
     module InstanceMethods
       def delete!(*args)
         ActsAsArchive.disable { self.delete(*args) }
       end
-      
+
       def destroy!(*args)
         ActsAsArchive.disable { self.destroy(*args) }
       end
     end
   end
-  
+
   module DatabaseStatements
     def self.included(base)
       unless base.included_modules.include?(InstanceMethods)
         base.send :include, InstanceMethods
         base.class_eval do
-          unless method_defined?(:delete_without_archive)
-            alias_method :delete_without_archive, :delete
-            alias_method :delete, :delete_with_archive
+          unless method_defined?(:delete_sql_without_archive)
+            alias_method :delete_sql_without_archive, :delete_sql
+            alias_method :delete_sql, :delete_sql_with_archive
           end
         end
       end
     end
-    
+
     module InstanceMethods
-      def delete_with_archive(arel, name=nil, binds=[])
+      def delete_sql_with_archive(sql, name = nil)
         @mutex ||= Mutex.new
         @mutex.synchronize do
           unless ActsAsArchive.disabled
-            sql = to_sql(arel)
-            
-            # Put the id into place.
-            # Line pulled from ActiveRecord::ConnectionAdapters::Mysql2Adapter.
-            # May not be compatible with other datastores.
-            sql =  sql.gsub("\0") { quote(*(binds.dup).shift.reverse) }
-            
             from, where = /DELETE FROM (.+)/i.match(sql)[1].split(/\s+WHERE\s+/i, 2)
             from = from.strip.gsub(/[`"]/, '').split(/\s*,\s*/)
-        
+
             ActsAsArchive.find(from).each do |config|
               ActsAsArchive.move(config, where)
             end
           end
         end
-        
-        delete_without_archive(arel, name, binds)
+
+        delete_sql_without_archive(sql, name)
       end
     end
   end
